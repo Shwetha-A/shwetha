dab4f0fdaa2fa45995f50a3f6f385d232c693171
Shwetha-A <116061230+Shwetha-A@users.noreply.github.com>
2022-11-10 15:44:52 +0530

    Add files via upload

--- array.java
+++ array.java
@@ -1,18 +1,52 @@
-package studentrecord;
+package commonelements;
+
+import java.io.*;
+import java.util.*;
 
 
 public class array {
 
-public static void main(String arg[])
-	    {
-	        String names[] = { "Rajesh", "Suresh", "Ramesh", "Kamlesh", "Vignesh" };
-	        int marks[] = { 45, 78, 83, 77, 93 };
-	        char sections[] = { 'A', 'B', 'A', 'A', 'B' };
-	        
-	        for(int i = 0; i < names.length; i++)
-	        {
-	            System.out.println( names[i] + " in section " + sections[i] + " got " + marks[i] + " marks." );
-	        }    
-	    }
-	
+			private static void FindCommonElemet(String[] arr1,
+										String[] arr2)
+	{
+		Set<String> set = new HashSet<>();
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr2.length; j++) {
+				if (arr1[i] == arr2[j]) {
+
+					// add common elements
+					set.add(arr1[i]);
+					break;
+				}
+			}
+		}
+		for (String i : set) {
+			System.out.print(i + " ");
+		}
+	}
+
+	// main method
+	public static void main(String[] args)
+	{
+
+		// create Array 1
+		String[] arr1
+			= { "Movie", "name", "is ", "Avatar-1"  };
+
+		// create Array 2
+		String[] arr2 = { "Movie", "name", "is ", "Avatar-2" };
+
+		// print Array 1
+		System.out.println("Array 1: "
+						+ Arrays.toString(arr1));
+
+		// print Array 2
+		System.out.println("Array 2: "
+						+ Arrays.toString(arr2));
+
+		System.out.print("Common Elements: ");
+
+		// Find the common elements
+		FindCommonElemet(arr1, arr2);
+	}
 }
